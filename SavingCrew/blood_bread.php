6d0f139c944826f073307757e53c452826bbc606
Lycanthrope <85288749+TaszidIzaz@users.noreply.github.com>
2022-04-20 00:09:49 +0600

    Updated the files of the homepage

---
+++ SavingCrew/blood_bread.php
@@ -0,0 +1,11 @@
+	
+		 <div class="row">
+            <div class="col-lg-12 col-xs-12">
+                <ol class="breadcrumb">
+                    <li><a href="Donor_reg.php" style='text-decoration:none;'><i class='fa fa-user-plus'></i> Donor Registration</a></li>
+                    <li><a href="request_blood.php" style='text-decoration:none;'><i class='fa fa-bed'></i> Request Donor </a></li>
+                    <li><a href="Search_Donor.php" style='text-decoration:none;'><i class='fa fa-search'></i> Search Donor</a></li>
+                </ol>
+            </div>
+        </div>
+		

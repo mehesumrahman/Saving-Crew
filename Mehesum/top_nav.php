0f6f28e909daf7eb738ffe22bc93f9585748a9d4
Mehesum99 <mehesumrahman@gmail.com>
2022-04-26 23:22:31 +0600

    Navigation Bar and Message view  Has been added

---
+++ Mehesum/top_nav.php
@@ -0,0 +1,27 @@
+   
+    <nav class="navbar navbar-default 	 navbar-fixed-top" role="navigation">
+        <div class="container">
+           
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><i class="fa fa-tint"></i> <b> Saving Crew </b> </a>
+            </div>
+         
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+                    <!-- <li><a href="about.php"><i class="fa fa-users"></i> About us</a></li> -->
+                    <li><a href="Donor_reg.php"><i class="fa fa-briefcase"></i> Services</a></li>
+                    <li><a href="contact.php"><i class="fa fa-envelope"></i> Contact us</a></li>
+                    <li><a href="admin.php"><i class="fa fa-user-md"></i> Admin</a></li>
+                </ul>
+            </div>
+         
+        </div>
+       
+    </nav>
